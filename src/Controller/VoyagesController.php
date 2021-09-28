e3861c3aae7694514d5bd7e6727e00dcf7b8699d
IbrahimArmanes <ibay411@hotmail.fr>
2021-09-28 16:02:41 +0200

    Phase 8 : Base de donnée

---
+++ src/Controller/VoyagesController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Controller;
+
+use App\Repository\VisiteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * Description of VoyagesController
+ *
+ * @author ibay4
+ */
+class VoyagesController extends AbstractController {
+    
+    /**
+     *
+     * @var VisiteRepository
+     */
+    private $repository;
+    
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    function __construct(VisiteRepository $repository) {
+        $this->repository = $repository;
+    }
+
+    
+    /**
+     * @Route ("/voyages", name="voyages")
+     * @return Response
+     */
+    public function index(): Response{
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+                ]);
+        
+}
+
+}
